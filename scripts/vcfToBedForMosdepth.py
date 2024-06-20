8029568a16002cc89b12264e0b9ca8acaaf1e2b2
Javier C.G <javierqnk@gmail.com>
2024-06-20 16:09:52 +0200

    Improved file handling

--- scripts/vcfToBedForMosdepth.py
+++ scripts/vcfToBedForMosdepth.py
@@ -5,8 +5,7 @@ from pysam import VariantFile
 def main(snakemake):
     vcfFile = snakemake.input.vcf
     bedFile = snakemake.output.bed
-    vcf = VariantFile(vcfFile)
-    with open(bedFile, "w") as bed:
+    with open(bedFile, "w") as bed, VariantFile(vcfFile) as vcf:
         for var in vcf.fetch():
             bed.write(f"{var.chrom}\t{var.pos - 61}\t{var.stop + 60}\t{var.id}\n")
 
